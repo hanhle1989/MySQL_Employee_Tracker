ba605764676be12aa24c60beb80703722802af98
Hanh Le <hanhdtle@gmail.com>
2021-03-18 22:59:49 -0700

    began tracker.js

---
+++ tracker.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password999!',
+  database: 'employee_db',
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+
+    start();
+})
+
